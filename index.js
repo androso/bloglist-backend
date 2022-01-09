c551d7ccbd4a9a6714e0db451cf48fd3498b6c60
Anibal Andrade <59257555+androso@users.noreply.github.com>
2022-01-09 15:48:24 +0000

    adding get and post handlers

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("let's see")
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const PORT = 3003;
+;
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://androso:JihyoMina@cluster0.42hyt.mongodb.net/bloglist?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.redirect("/api/blogs");
+})
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
